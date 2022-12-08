ced559016afb8323fef50c2192111749d5bad4af
AndriyNich <info@bembi.ua>
2022-12-09 00:34:42 +0200

    6

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+;(() => {
+  document
+    .querySelector('#validation-input')
+    .addEventListener('blur', function () {
+      this.classList = []
+      if (this.value.length === parseInt(this.dataset.length)) {
+        this.classList.add('valid')
+      } else if (this.value.length !== 0) {
+        this.classList.add('invalid')
+      }
+    })
+})()
