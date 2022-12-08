3d9398099d17b9d5270ccdd3151ad28fef33d6f2
AndriyNich <info@bembi.ua>
2022-12-09 00:45:39 +0200

    7

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+;(() => {
+  document
+    .querySelector('#font-size-control')
+    .addEventListener('input', function () {
+      document.querySelector('#text').style.fontSize = this.value + 'px'
+    })
+})()
