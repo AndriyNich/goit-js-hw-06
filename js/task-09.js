dea7b115fa821131b53e09ec39003b40b9f63eba
AndriyNich <info@bembi.ua>
2022-12-10 01:16:59 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
 }
+
+document.querySelector('button.change-color').addEventListener('click', () => {
+  const backgroundColor = getRandomHexColor()
+  document.body.style.backgroundColor = backgroundColor
+  document.querySelector('span.color').textContent = backgroundColor
+})
