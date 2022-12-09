264a969374e74029b6a6dd1014217f11dc33f955
AndriyNich <info@bembi.ua>
2022-12-10 00:34:39 +0200

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+;(() => {
+  const form = document.querySelector('.login-form')
+  form.addEventListener('submit', callbackSubmit)
 
+  function callbackSubmit(event) {
+    event.preventDefault()
+
+    const {
+      elements: {email, password},
+    } = this
+
+    const elements = {
+      email: email.value,
+      password: password.value,
+    }
+
+    console.log(elements)
+
+    if (email.value === '' || password.value === '') {
+      alert('Please fill in all the fields!')
+    } else {
+      event.currentTarget.reset()
+    }
+  }
+})()
