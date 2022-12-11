d703e04ea5209d58ad5bd4379ffb57f819f3daf5
AndriyNich <info@bembi.ua>
2022-12-11 15:25:40 +0200

    pre-finish2

--- js/task-10.js
+++ js/task-10.js
@@ -1,18 +1,7 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
-}
-
-const getRandomValidHexColor = () => {
-  let color = getRandomHexColor()
-  if (color.length === 7) {
-    return color
-  }
-
-  return [
-    color[0],
-    ...Array(7 - color.length).fill(0),
-    ...color.slice(1).split(''),
-  ].join('')
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`
 }
 
 const outputContainer = document.querySelector('#boxes')
@@ -22,7 +11,7 @@ outputContainer.style.gap = '10px'
 
 const createElement = (element, index) => {
   const divRef = document.createElement('div')
-  divRef.style.backgroundColor = getRandomValidHexColor()
+  divRef.style.backgroundColor = getRandomHexColor()
   divRef.style.width = 30 + 10 * index + 'px'
   divRef.style.height = divRef.style.width
   return divRef
