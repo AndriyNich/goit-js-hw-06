b3379f94fdd3fdcb1bd3ff02a4d86d836fe89ef4
AndriyNich <info@bembi.ua>
2022-12-12 17:46:04 +0200

    pre-finish3

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,14 @@
 const categories = document.querySelectorAll('#categories > .item')
 
 console.log(`Number of categories: ${categories.length}`)
-console.log('')
 
 const result = categories =>
   categories.forEach(category => {
-    console.log('Category: ', category.querySelector('h2').textContent)
+    console.log('\nCategory: ', category.querySelector('h2').textContent)
     const numberElements = console.log(
       'Elements: ',
       category.querySelectorAll('ul > li').length,
     )
-    console.log('')
   })
 
 result(categories)
--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,8 @@
 const input = document.querySelector('#name-input')
 const output = document.querySelector('#name-output')
 
-const callback = function (element) {
-  element.textContent = this.value.length === 0 ? 'Anonymous' : this.value
+const callback = function () {
+  output.textContent = this.value.length === 0 ? 'Anonymous' : this.value
 }
 
-input.addEventListener('input', callback.bind(input, output))
+input.addEventListener('input', callback)
